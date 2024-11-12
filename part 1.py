37b0fb25b376e1ea37d4da63a1a3768177948041
Thaddycat <thaddeuslehmer@yahoo.com>
2024-11-12 11:14:32 -0800

    initial commit

---
+++ part 1.py
@@ -0,0 +1,29 @@
+# catelog the characteristics of a book(s)
+
+1. Identify the book(s) to use as key
+2.decide how many lines make a page, and how many characters make a line
+3. read the book line by line and clean each line:
+
+def clean_line(line):
+    return line.strip().replace
+
+
+The eagle has landed.
+  Z A             X
+
+Book : key
+
+page = {
+    1: {
+        1: "this is line 1 of page 1"
+        2: "this is line 2 of page 1"
+    }
+    2: {
+        1:"this is line 1 of page 2"
+        2:"this is line 2 of page 2"
+    }
+}
+
+my_dict = {
+    'e':
+}
